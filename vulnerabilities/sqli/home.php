1128ffbb7bd5941c72d759fcec1458617b8d2cb9
Subho Halder <subho.halder@gmail.com>
2019-11-23 19:45:04 +0530

    are you kidding

--- vulnerabilities/sqli/home.php
+++ vulnerabilities/sqli/home.php
@@ -48,11 +48,11 @@
                             $item = isset($_POST['item']) ? $_POST['item'] : '';
                             $search = isset($_POST['search']) ? $_POST['search'] : '';
                             $isSearch = false;
-                            if(($item!="OR") || ($item!="AND") || ($item!="or") || ($item!="and")) {
+                            if(($item=="NULL") || ($item=="SELECT") || ($item=="WHERE") || ($item=="UNION") || ($item=="null") || ($item=="select") || ($item=="where") || ($item=="union")) {
                                 echo "<br><ul class=\"featureList\">";
                                 echo "<li class=\"cross\">Error! Are you trying to hack?.</li>";
                                 echo "</ul>";
-                            }else if(($search!="OR") || ($search!="AND") || ($search!="or") || ($search!="and")) {
+                            }else if(($search=="NULL") || ($search=="SELECT") || ($search=="WHERE") || ($search=="UNION") || ($search=="null") || ($search=="select") || ($search=="where") || ($search=="union")) {
                                 echo "<br><ul class=\"featureList\">";
                                 echo "<li class=\"cross\">Error! Are you trying to hack?.</li>";
                                 echo "</ul>";
